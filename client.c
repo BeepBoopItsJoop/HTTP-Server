d9fee8532691dd166c9dc24bd5a9fa3180c6fcaf
Radoslav Ivanov <BeepBoopItsTheJoop@gmail.com>
2025-01-08 16:43:39 +0200

    Server now receives a request and sends dummy http response

---
+++ client.c
@@ -0,0 +1,6 @@
+// get address info
+// find first available socket
+// connect 
+// receive
+// do other stuff
+// close
--- server.c
+++ server.c
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 // #include <sys/types.h>
-// #include <sys/socket.h>
+#include <sys/socket.h>
 #include <netdb.h>
 #include <unistd.h>
 #include <signal.h>
 #include <errno.h>
+#include <arpa/inet.h>
 #include <sys/wait.h>
 
 #define PORT "3490"
 #define BACKLOG 10
+#define BUFF_SIZE 1024
 
 void sigchld_handler(int s)
 {
@@ -22,6 +24,34 @@ void sigchld_handler(int s)
     errno = saved_errno;
 }
 
+// get sockaddr, IPv4 or IPv6:
+void* get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int sendall(int s, const char* buf, size_t* len) {
+     int totalsent = 0;
+     int bytesleft = *len;
+     int n;
+
+     while(totalsent < *len) {
+          n = send(s, buf+totalsent, bytesleft, 0);
+          if(n == -1) break;
+          
+          totalsent += n; 
+          bytesleft -= n; 
+     }
+
+     *len = totalsent;
+
+     return (n == -1) ? -1 : 0;
+}
+
 int main(void) {
      struct addrinfo hints, *servinfo;
      int rv;
@@ -29,13 +59,15 @@ int main(void) {
      memset(&hints, 0, sizeof(hints));
      hints.ai_family = AF_UNSPEC;
      hints.ai_socktype = SOCK_STREAM;
-     hints.ai_flags = AI_PASSIVE;
+     hints.ai_flags = AI_PASSIVE; // Use machine IP
 
+     // get address info for this host machine
      if((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
           fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
           return 1;
      }
 
+     // loop through results and bind to the first one we can
      int sockfd;
      struct addrinfo* p;
      for(p = servinfo; p != NULL; p = p->ai_next) {
@@ -45,6 +77,7 @@ int main(void) {
                continue;
           }
 
+          // configure socket info
           int yes = 1; 
           int setres = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
           if (setres == -1) {
@@ -75,6 +108,7 @@ int main(void) {
           exit(1);
      }
 
+     // black magic
      struct sigaction sa;
      sa.sa_handler = sigchld_handler; // reap all dead processes
      sigemptyset(&sa.sa_mask);
@@ -86,5 +120,52 @@ int main(void) {
 
     printf("server: waiting for connections...\n");
 
+     // 
+     int new_fd;
+     struct sockaddr_storage client_addr;
+     socklen_t sin_size = sizeof(client_addr);
+     char s [INET6_ADDRSTRLEN];
+     while(1) {
+          new_fd = accept(sockfd, (struct sockaddr *)&client_addr, &sin_size);
+          if(new_fd == -1) {
+               perror("accept");
+               continue;
+          }
+
+          inet_ntop(client_addr.ss_family, 
+               get_in_addr((struct sockaddr *)&client_addr), 
+               s, sizeof(s));
+          printf("server: got connection from %s\n", s);
+
+          if(!fork()) {
+               close(sockfd);
+
+               // TODO: do something about buffer overflow
+               char buffer[BUFF_SIZE] = {0};
+               recv(new_fd, buffer, BUFF_SIZE, 0);
+               printf("Request: %s\n", buffer);
+
+               // TODO: Implement handling html file serving
+               const char* response = 
+                    "HTTP/0.9 200 OK\r\n"
+                    "Content-Type: text/plain\r\n\r\n"
+                    "Hello, World!";
+               size_t reslen = strlen(response);     
+               sendall(new_fd, response, &reslen);
+
+               // char* str = "Hello, world!, again\n";
+               // size_t len = strlen(str);
+               // if (sendall(new_fd, str, &len) == -1) {
+               //      perror("sendall");
+               //      printf("Only %zu bytes of data were sent before an error!\n", len);
+               // }
+
+               close(new_fd);
+               exit(0);
+          }
+
+          close(new_fd);
+     }
+     return 0;
 }
 
